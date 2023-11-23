e6124d0d4d0a0157ecd89b547ef6568d5c7bfe2f
JaeHoon <wogns8030@naver.com>
2023-11-23 10:11:37 +0900

    jpa,security,web,lombok.mysql

---
+++ src/main/java/com/sparta/backendgram/BackendgramApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.backendgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendgramApplication.class, args);
+    }
+
+}
