c98784e0fa05f5e5f43bc6adf6d02c2ddfaf56f1
JaeHoon <wogns8030@naver.com>
2023-11-26 02:48:45 +0900

    기본

---
+++ src/main/java/com/example/b08backendgram/B08backendgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.b08backendgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B08backendgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B08backendgramApplication.class, args);
+    }
+
+}
